609dc5ef6753756abd209e680d16183b46f9035a
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2025-01-27 14:40:32 +0000

    created internet gateway

--- project-1_creating-linux-server-on-aws/main.tf
+++ project-1_creating-linux-server-on-aws/main.tf
@@ -6,3 +6,23 @@ resource "aws_vpc" "project-1-vpc" {
     Name = "project-1-vpc"
   }
 }
+
+# Create a subnet
+resource "aws_subnet" "project-1-subnet" {
+  vpc_id            = aws_vpc.project-1-vpc.id
+  cidr_block        = var.cidr_block
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "project-1-subnet-public"
+  }
+}
+
+# Create an internet gateway
+resource "aws_internet_gateway" "project-1-igw" {
+  vpc_id = aws_vpc.project-1-vpc.id
+
+  tags = {
+    Name = "project-1-igw"
+  }
+}
